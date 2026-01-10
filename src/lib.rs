ba2289d4a67a6ba651b3fafc315138532cc05c3a
nyankyu <fukujinn@gmail.com>
2026-01-10 23:02:32 +0900

    initialization process was made into a function

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,7 @@
 #![no_main]
 pub mod allocator;
 pub mod graphics;
+pub mod init;
 pub mod qemu;
 pub mod result;
 pub mod serial;
@@ -20,12 +21,9 @@ fn efi_main(
     image_handle: uefi::EfiHandle,
     efi_system_table: &uefi::EfiSystemTable,
 ) {
-    let mut memory_map = uefi::MemoryMapHolder::new();
-    uefi::exit_from_efi_boot_services(
+    init::init_basic_runtime(
         image_handle,
         efi_system_table,
-        &mut memory_map,
     );
-    allocator::ALLOCATOR.init_with_mmap(&memory_map);
     run_unit_tests();
 }
--- src/main.rs
+++ src/main.rs
@@ -6,14 +6,13 @@ use core::panic::PanicInfo;
 use core::writeln;
 
 use wasabi::graphics::draw_test;
+use wasabi::init::init_basic_runtime;
 use wasabi::qemu::QemuExitCode;
 use wasabi::qemu::exit_qemu;
 use wasabi::uefi::EfiHandle;
 use wasabi::uefi::EfiMemoryType;
 use wasabi::uefi::EfiSystemTable;
-use wasabi::uefi::MemoryMapHolder;
 use wasabi::uefi::VramTextWriter;
-use wasabi::uefi::exit_from_efi_boot_services;
 use wasabi::uefi::init_vram;
 
 use wasabi::x86::hlt;
@@ -34,11 +33,8 @@ fn efi_main(
         writeln!(w, "This is line {i}").unwrap();
     }
 
-    let mut memory_map = MemoryMapHolder::new();
-    let status = efi_system_table
-        .boot_services()
-        .get_memory_map(&mut memory_map);
-    writeln!(w, "{status:?}").unwrap();
+    let memory_map =
+        init_basic_runtime(image_handle, efi_system_table);
 
     let mut total_memory_pages = 0;
 
@@ -61,12 +57,6 @@ fn efi_main(
         total_memory_pages, total_memory_size
     )
     .unwrap();
-
-    exit_from_efi_boot_services(
-        image_handle,
-        efi_system_table,
-        &mut memory_map,
-    );
 
     writeln!(w, "Hello, Non-UEFI world!").unwrap();
 
